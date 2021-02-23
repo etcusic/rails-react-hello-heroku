48dcdef0157beabfc4d1bce311d82dc1b8379bb1
Eli Cusic <tntitans31@gmail.com>
2021-02-23 14:39:56 -0600

    set up package.json in client directory and config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :plants
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :plants
+    end
+  end
 end
